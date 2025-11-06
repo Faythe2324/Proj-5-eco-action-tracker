bea622583a9ff6409828c97bdbee6052d0013107
Faythe2324 <faythemagsombol23@gmail.com>
2025-11-07 01:28:36 +0800

    challenge list

---
+++ Eco action/UC5_Join_Community_Challenges/UC5_1Challenge_list.py
@@ -0,0 +1,9 @@
+class ChallengeList:
+    def get_list(self):
+        return [
+            ("Zero Waste Week", 100),
+            ("Bike 50 km Challenge", 250),
+            ("Plant 5 Trees", 50),
+            ("Water Conservation", 150),
+            ("Power Saver Challenge", 200)
+        ]
